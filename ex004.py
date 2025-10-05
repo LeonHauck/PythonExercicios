dc61a190fc5f4776236677684b2d201108ed9083
Leon "Guts" Hauck <leonhauck98@gmail.com>
2025-10-05 12:55:12 -0300

    Day Four

---
+++ ex004.py
@@ -0,0 +1,14 @@
+n = input('Digite um valor:')
+print(type(n.isalnum()))
+print(n.isalpha())
+print(n.isdigit())
+print(n.isspace())
+print(n.isupper())
+print(n.islower())
+print(n.isnumeric())
+
+
+
+
+
+
