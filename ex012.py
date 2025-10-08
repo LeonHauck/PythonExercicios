d0b2a105f17ee3cd40ffe8635e4d85d3c535684a
Leon "Guts" Hauck <leonhauck98@gmail.com>
2025-10-07 22:21:22 -0300

    Day six

--- ex012.py
+++ ex012.py
@@ -1,7 +1,8 @@
-p = int(input('Digite o valor do produto:'))
+p = float(input('Digite o valor do produto:'))
 d = p * 0.05
 vf = p - d
 print(f'O preço do produto e de R$ {p} com o desconto de 5% seu preço vai ser de R$ {vf:.2f}')
+
 
 
 
