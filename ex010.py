c8b9217ca0363f4642eeebf8ab426716ea936f74
Leon "Guts" Hauck <leonhauck98@gmail.com>
2025-10-07 22:11:33 -0300

    Day six

--- ex010.py
+++ ex010.py
@@ -1,5 +1,6 @@
-n = float(input('Digite a quantida de dinheiro que você possui'))
-print(f'O dinheiro total que você possui é {n} \n sendo assim o total de dolares que você pode comprar e {n / 3.27:.2f}')
+n = float(input('Quanto dinheiro você possui na carteira ? '))
+print(f'O dinheiro total que você possui na carteira é de R$ {n} \n sendo assim o total de dolares que você pode comprar e US$ {n / 3.27:.2f}')
+
 
 
 
