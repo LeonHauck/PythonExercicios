ad5530665d2e5fdb47193e2311009c41e8918225
Leon "Guts" Hauck <leonhauck98@gmail.com>
2025-10-07 21:49:59 -0300

    Day six

--- ex006.py
+++ ex006.py
@@ -1,5 +1,6 @@
 n1 = int(input('Digite um número'))
-print(f'O número que você digitou foi {n1}, \n o dobro deste número é {n1 * 2}  \n o triplo deste número é {n1 * 3}  \n e a raiz quadrada deste número é {n1 ** 2}')
+print(f'O número que você digitou foi {n1}, \n o dobro deste número é {n1 * 2}  \n o triplo deste número é {n1 * 3}  \n e a raiz quadrada deste número é {n1 ** (1/2):.2f}')
+
 
 
 
